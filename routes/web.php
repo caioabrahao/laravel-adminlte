a61a1c95eab8fb9cc6b917927fb5570e2831fbdf
Caio Abrahão <contato.caioabrahao@gmail.com>
2025-11-07 16:05:27 -0300

    feat/ Configured routes for Authentication. Styld login pages as AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\CompanyController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/admin', [DashboardController::class, 'index']);
-Route::resource('companies', CompanyController::class);
+Route::get('/admin', [DashboardController::class, 'index']); # test purposes (delete later)
 
 Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('companies', CompanyController::class);
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
